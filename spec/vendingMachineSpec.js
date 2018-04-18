00bba928357a4feff33e098f42c75ffd69887d3b
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-04-18 15:07:43 +0100

    Add buy cola, candy a nd chips functions

--- spec/vendingMachineSpec.js
+++ spec/vendingMachineSpec.js
@@ -18,9 +18,25 @@ describe("Feature test", function() {
     expect(vendingMachine.currentBalance).toBe(0);
   });
 
+  it("returns the currentBalance", function() {
+    expect(vendingMachine.initialBalance).toBe(0);
+  });
+
   it("displays the products with the price", function() {
     expect(vendingMachine.displayProducts(cola, candy, chips)).toBe(
       "Products: cola = $1, candy = $0.5, chips = $0.65."
     );
+  });
+
+  it("returns current balance after buy a cola", function() {
+    expect(vendingMachine.buyCola(cola)).toBe(1);
+  });
+
+  it("returns current balance after buy a candy", function() {
+    expect(vendingMachine.buyCandy(candy)).toBe(0.65);
+  });
+
+  it("returns current balance after buy a chips", function() {
+    expect(vendingMachine.buyChips(chips)).toBe(0.5);
   });
 });
--- src/vendingMachine.js
+++ src/vendingMachine.js
@@ -2,6 +2,7 @@ function VendingMachine() {
   var cola = new Cola();
   var chips = new Chips();
   var candy = new Candy();
+  this.initialBalance = 0;
   this.currentBalance = 0;
 }
 
@@ -16,6 +17,18 @@ VendingMachine.prototype.displayProducts = function(cola, chips, candy) {
     chips.price +
     "."
   );
+};
+
+VendingMachine.prototype.buyCola = function(cola) {
+  return (this.currentBalance += cola.price);
+};
+
+VendingMachine.prototype.buyCandy = function(candy) {
+  return (this.currentBalance += candy.price);
+};
+
+VendingMachine.prototype.buyChips = function(chips) {
+  return (this.currentBalance += chips.price);
 };
 
 // VendingMachine.prototype.displaySoldOut = function(arguments) {};
