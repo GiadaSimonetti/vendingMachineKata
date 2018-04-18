03a9a5eba521db8906837b49c03e4817f6a613a5
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-04-18 12:02:45 +0100

    Add display product function

--- spec/vendingMachineSpec.js
+++ spec/vendingMachineSpec.js
@@ -1,22 +1,27 @@
 describe("Feature test", function() {
-  // var cola;
-  // var candy;
-  // var chips;
+  var cola;
+  var candy;
+  var chips;
   var vendingMachine;
-  var products;
+  // var products;
 
   beforeEach(function() {
-    // cola = new Cola();
-    // candy = new Candy();
-    // chips = new Chips();
-    products = new Products();
-    vendingMachine = new VendingMachine(products);
+    cola = new Cola();
+    candy = new Candy();
+    chips = new Chips();
+    // products = new Products();
+    vendingMachine = new VendingMachine();
   });
+  it("returns the cola item", function() {
+    expect(cola.price).toBe(1.0);
+  });
+
   it("returns the currentBalance", function() {
     expect(vendingMachine.currentBalance).toBe(0);
   });
+
   it("displays the products with the price", function() {
-    expect(vendingMachine.displayProducts(products)).toBe(
+    expect(vendingMachine.displayProducts(cola, candy, chips)).toBe(
       "Products: cola = $1, candy = $0.5, chips = $0.65."
     );
   });
